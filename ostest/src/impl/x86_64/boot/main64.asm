236d067d47b2e1ce55c0df648c96ba1c57b1df6c
AFellowSpeedrunner/MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2022-08-02 01:29:28 +0100

    Update main64.asm

--- ostest/src/impl/x86_64/boot/main64.asm
+++ ostest/src/impl/x86_64/boot/main64.asm
@@ -1,8 +1,12 @@
 global long_mode_start
+; referencing our C file
 extern kernel_main
 
 section .text
+; ohhh yeah 64 bit mode!
 bits 64
+; 1. im forgetting what this does
+; 2. im assuming it's allowing us to run C files
 long_mode_start:
     mov ax, 0
     mov ss, ax
@@ -10,6 +14,18 @@ long_mode_start:
     mov es, ax
     mov fs, ax
     mov gs, ax
-
+    
+    ; and here on out everything is handled by C
     call kernel_main
+    ; sadly though i didn't think so far through
+    ; so no matter what i put in i don't know if
+    ; it even works idk if it does something else
+    ; or it halts as ive done testing attempting
+    ; to run a kinda input but it did nothing
+    ; so essentially i think it was bad programming
+    ; or i need to do "bug fix"
     hlt
+
+
+; well done! now you know our kernel works!
+; have fun modifying it!
